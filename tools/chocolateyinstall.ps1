d3ee23499e8c8c69c2258ab28e1623a6ce763405
John Burak <john.burak@gmail.com>
2016-03-10 21:14:53 -0700

    WIP

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,106 @@
+﻿$ErrorActionPreference = 'Stop';
+
+# Gather choco install args
+# Default values
+$createDesktopIcon     = $true
+$createQuickLaunchIcon = $true
+$addContextMenuFiles   = $true
+$addContextMenuFolders = $true
+$addToPath             = $true
+
+$arguments = @{}
+$packageParameters = $env:chocolateyPackageParameters
+if ($packageParameters)
+{
+      $match_pattern = "\/(?<option>([a-zA-Z]+)):(?<value>([`"'])?([a-zA-Z0-9- _\\:\.]+)([`"'])?)|\/(?<option>([a-zA-Z]+))"
+      $option_name = 'option'
+	  $value_name = 'value'
+      if ($packageParameters -match $match_pattern )
+      {
+          $results = $packageParameters | Select-String $match_pattern -AllMatches
+          $results.matches | % {
+            $arguments.Add(
+                $_.Groups[$option_name].Value.Trim(), 
+                $_.Groups[$value_name].Value.Trim())
+          }
+      }
+      else
+        { Throw "Package Parameters were found but were invalid (REGEX Failure)" }
+
+      if ($arguments.ContainsKey("nodesktopicon"))
+      {
+          Write-Host "nodesktopicon"
+          $createDesktopIcon = $false
+      }
+
+      if ($arguments.ContainsKey("noquicklaunchicon")) 
+      {
+          Write-Host "noquicklaunchicon"
+          $createQuickLaunchIcon = $false
+      }
+
+      if ($arguments.ContainsKey("nocontextmenufiles")) 
+      {
+          Write-Host "nocontextmenufiles"
+          $addContextMenuFiles = $false
+      }
+
+      if ($arguments.ContainsKey("nocontextmenufolders")) 
+      {
+          Write-Host "nocontextmenufolders"
+          $addContextMenuFolders = $false
+      }
+      
+      if ($arguments.ContainsKey("dontaddtopath")) 
+      {
+          Write-Host "dontaddtopath"
+          $addToPath = $false
+      }
+}
+else
+    { Write-Debug "No Package Parameters Passed in" }
+
+# Format install args for the Visual Studio Code installer
+$mergeTasks = "!runCode"
+if ($createDesktopIcon)     { $mergeTasks = $mergeTasks + ",desktopicon"} 
+if ($createQuickLaunchIcon) { $mergeTasks = $mergeTasks + ",quicklaunchicon" }
+if ($addContextMenuFiles)   { $mergeTasks = $mergeTasks + ",addcontextmenufiles" }
+if ($addContextMenuFolders) { $mergeTasks = $mergeTasks + ",addcontextmenufolders" }
+if ($addToPath)             { $mergeTasks = $mergeTasks + ",addtopath" }
+
+$packageName= 'VisualStudioCodeInsider'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://az764295.vo.msecnd.net/insider/5b5f4db87c10345b9d5c8d0bed745bcad4533135/VSCodeSetup-insider.exe'
+#$url64      = ''
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+
+  silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /mergetasks=$mergeTasks /log=`"$env:temp\vscode.log`"" # Inno Setup, plus args for Visual Studio Code installer
+  validExitCodes= @(0, 3010, 1641)
+
+  softwareName  = 'Visual Studio Code Insiders*'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
