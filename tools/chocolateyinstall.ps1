4a0aba7bc7f1e49bd07b45a33de918bf19cd15f3
PSVortex <31849821+PSVortex@users.noreply.github.com>
2018-04-24 19:21:55 +0200

    added x64, update links

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -70,8 +70,8 @@ if ($addToPath)             { $mergeTasks = $mergeTasks + ",addtopath" }
 
 $packageName= 'VisualStudioCode-Insiders'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://az764295.vo.msecnd.net/insider/ee9d91c05fa571de56c41383cceee30b05b0a2f6/VSCodeSetup-1.9.0-insider.exe'
-#$url64      = ''
+$url        = 'https://vscode-update.azurewebsites.net/latest/win32/insider'
+$url64      = 'https://vscode-update.azurewebsites.net/latest/win32-x64/insider'
 
 $packageArgs = @{
   packageName   = $packageName
